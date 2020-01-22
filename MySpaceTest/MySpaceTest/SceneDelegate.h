3c21bbb051222e13b5e0774bd278f94438b7c88e
Joker <1195036850@qq.com>
2020-01-22 10:40:42 +0800

    新项目第一版

---
+++ MySpaceTest/MySpaceTest/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  MySpaceTest
+//
+//  Created by Stack on 2020/1/22.
+//  Copyright © 2020 Stack. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
---
+++ MySpaceTest/MySpaceTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MySpaceTest
+//
+//  Created by Stack on 2020/1/22.
+//  Copyright © 2020 Stack. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
